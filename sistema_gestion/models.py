5d520757160eb808e9b36ddfc15e399d6a689ed8
Gesbien <20190416@ce.pucmm.edu.do>
2023-01-17 23:44:10 -0400

    Creacion del Proyecto Añadidos Creacion del proyecto Creacion de la app Creacion de los modelos persona e informacion

---
+++ sistema_gestion/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class persona(models.Model):
+    id_persona = models.IntegerField(unique= True)
+    cedula = models.IntegerField(unique= True)
+    nombres = models.CharField(max_length= 80,unique=True)
+    primer_apellido = models.CharField(max_length=40)
+    segundo_apellido = models.CharField(max_length=40)
+    tipo = models.CharField(max_length=40)
+    estado = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.id_persona
+
+class informacion(models.Model):
+    id_informacion = models.IntegerField(unique= True)
+    id_persona = models.IntegerField(unique= True)
+    tipo = models.CharField(max_length= 40)
+    descripcion = models.CharField(max_length = 128)
+    estado = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.id_informacion
