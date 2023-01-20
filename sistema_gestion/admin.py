6342748fd1d19b1ff37e660cf5c0c7e34c4d924b
Gesbien <20190416@ce.pucmm.edu.do>
2023-01-19 23:13:55 -0400

    Update Creacion del modelo de Solicitud

--- sistema_gestion/admin.py
+++ sistema_gestion/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from sistema_gestion.models import solicitud,persona,informacion
 
 # Register your models here.
+admin.site.register(solicitud)
+admin.site.register(persona)
+admin.site.register(informacion)
--- sistema_gestion/models.py
+++ sistema_gestion/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class persona(models.Model):
-    id_persona = models.IntegerField(unique= True)
+    id_persona = models.IntegerField(primary_key= True)
     cedula = models.IntegerField(unique= True)
     nombres = models.CharField(max_length= 80,unique=True)
     primer_apellido = models.CharField(max_length=40)
@@ -14,7 +14,7 @@ class persona(models.Model):
         return self.id_persona
 
 class informacion(models.Model):
-    id_informacion = models.IntegerField(unique= True)
+    id_informacion = models.IntegerField(primary_key= True)
     id_persona = models.IntegerField(unique= True)
     tipo = models.CharField(max_length= 40)
     descripcion = models.CharField(max_length = 128)
@@ -22,3 +22,10 @@ class informacion(models.Model):
 
     def __str__(self):
         return self.id_informacion
+class solicitud(models.Model):
+    id_solicitud = models.IntegerField(primary_key = True)
+    id_persona = models.IntegerField()
+    estado = models.CharField(max_length= 40)
+    monto = models.FloatField()
+    tasa = models.FloatField()
+    cuota = models.IntegerField()
