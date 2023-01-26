3e6deb325c7ddbeafb4da57e806008144fe1779d
Gesbien <20190416@ce.pucmm.edu.do>
2023-01-26 17:27:11 -0400

    Cambios en Modelos Modificacion de foreignkey en modelos informacion y solicitud

--- sistema_gestion/models.py
+++ sistema_gestion/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class persona(models.Model):
-    id_persona = models.IntegerField(primary_key= True)
+    id_persona = models.AutoField(primary_key= True)
     cedula = models.IntegerField(unique= True)
-    nombres = models.CharField(max_length= 80,unique=True)
+    nombres = models.CharField(max_length= 80)
     primer_apellido = models.CharField(max_length=40)
     segundo_apellido = models.CharField(max_length=40)
     tipo = models.CharField(max_length=40)
@@ -14,8 +14,8 @@ class persona(models.Model):
         return self.id_persona
 
 class informacion(models.Model):
-    id_informacion = models.IntegerField(primary_key= True)
-    id_persona = models.IntegerField(unique= True)
+    id_informacion = models.AutoField(primary_key= True)
+    id_persona =  models.ForeignKey(persona,null=True,on_delete=models.CASCADE)
     tipo = models.CharField(max_length= 40)
     descripcion = models.CharField(max_length = 128)
     estado = models.CharField(max_length=40)
@@ -23,8 +23,8 @@ class informacion(models.Model):
     def __str__(self):
         return self.id_informacion
 class solicitud(models.Model):
-    id_solicitud = models.IntegerField(primary_key = True)
-    id_persona = models.IntegerField()
+    id_solicitud = models.AutoField(primary_key = True)
+    id_persona = models.ForeignKey(persona,null=True,on_delete=models.CASCADE)
     estado = models.CharField(max_length= 40)
     monto = models.FloatField()
     tasa = models.FloatField()
