999d28fff57ebf1c50da69749e9a3276139a46c2
Gesbien <20190416@ce.pucmm.edu.do>
2023-02-08 08:47:05 -0400

    UPDATE Modificaciones del CRUD de Solicitudes

--- sistema_gestion/urls.py
+++ sistema_gestion/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 from . import personas
 from . import solicitud
 
 urlpatterns = [
+    path('admin/',admin.site.urls),
     path('',views.inicio,name='inicio'),
     path('personas',solicitud.inicio_solicitud,name='inicio_solicitud'),
     path('personas/crear',personas.create,name='crear'),
