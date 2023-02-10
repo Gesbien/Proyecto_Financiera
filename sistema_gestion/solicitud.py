e4d88ffe2cbec74139f8dca6839dd2de60238f4a
Gesbien <20190416@ce.pucmm.edu.do>
2023-02-09 23:06:50 -0400

    UPDATE Modificaciones al CRUD de Solicitud +Edicion de Solicitud

--- sistema_gestion/solicitud.py
+++ sistema_gestion/solicitud.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import solicitud
 from .models import persona
 
@@ -19,4 +19,34 @@ def registroSolicitud(request):
 
     Solicitud = solicitud.objects.create(id_solicitud=id_solicitud, estado=estado, monto=monto,
                                          tasa=tasa, cuota=cuota)
-    return redirect('registrarSolicitud/')
+    return redirect('/solicitud')
+
+def editarSolicitud(request, id_solicitud):
+    Solicitud = solicitud.objects.get(id_solicitud=id_solicitud)
+    data = {
+        'solicitud': Solicitud
+    }
+    return render(request, "paginas/edicionSolicitud.html", data)
+
+def edicionSolicitud(request):
+    id_solicitud = request.POST['txtId_Solicitud']
+    estado = request.POST['txtEstado']
+    monto = request.POST['numMonto']
+    tasa = request.POST['numTasa']
+    cuota = request.POST['numCuota']
+
+    Solicitud = solicitud.objects.get(id_solicitud=id_solicitud)
+    Solicitud.estado = estado
+    Solicitud.monto = monto
+    Solicitud.tasa = tasa
+    Solicitud.cuota = cuota
+    Solicitud.save()
+
+    return redirect('/solicitud')
+
+def eliminacionSolicitud(request, id_solicitud):
+    Solicitud = solicitud.objects.get(id_solicitud=id_solicitud)
+    Solicitud.estado = 'Anulado'
+    Solicitud.save()
+
+    return redirect('/solicitud')
--- sistema_gestion/urls.py
+++ sistema_gestion/urls.py
@@ -5,16 +5,12 @@ from . import personas
 from . import solicitud
 
 urlpatterns = [
-    path('admin/',admin.site.urls),
     path('',views.inicio,name='inicio'),
-    path('personas',solicitud.inicio_solicitud,name='inicio_solicitud'),
-    path('personas/crear',personas.create,name='crear'),
-    path('personas/editar',personas.edit,name='editar'),
-    path('personas/eliminar',personas.delete,name='eliminar'),
-    path('registrarSolicitud/',solicitud.crear_solicitud,name='registrarSolicitud'),
-    path('registroSolicitud/',solicitud.registroSolicitud,name='registroSolicitud'),
-    path('edicionSolicitud/<id_solicitud>',views.edicionSolicitud),
-    path('editarSolicitud/',views.editarSolicitud),
-    path('eliminacionSolicitud/',views.eliminacionSolicitud)
+    path('solicitud',solicitud.inicio_solicitud,name='inicio_solicitud'),
+    path('solicitud/registrar/',solicitud.crear_solicitud,name='registrarSolicitud'),
+    path('solicitud/registro/',solicitud.registroSolicitud,name='registroSolicitud'),
+    path('solicitud/editar/<id_solicitud>',solicitud.editarSolicitud,name='editarSolicitud'),
+    path('solicitud/edicion/',solicitud.edicionSolicitud,name='edicionSolicitud'),
+    path('solicitud/anular/<id_solicitud>',solicitud.eliminacionSolicitud,name='anularSolicitud'),
 
 ]
